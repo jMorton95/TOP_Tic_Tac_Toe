6d5c08490ea666c4155d4931d30b90e82d8d2d03
unknown <mortson@hotmail.co.uk>
2022-07-04 15:20:30 +0100

    Updated JS References

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const SelectorMenu = (() => {
+const GameSetup = (() => {
   const players = [];
   const gameBoard = [];
-  const _buttons = document.querySelectorAll('.check');
+  const _startButton = document.querySelector('.startButton');
 
   const _Player = (playerName, e) => {
     //Factory Function that Returns a Player Object and stores it in our Players Array.
@@ -17,37 +17,30 @@ const SelectorMenu = (() => {
    
   function _startGame(){
     //Create and Append 9 GameBoard Section Elements.
-    for (let x = 9; x > 0; x--){
+    for (let x = 0; x < 9; x++){
         const element = document.createElement('div');
         element.classList.add('gbSection');
         gameBoard.push(element);
       }
 
-    let main = document.querySelector('mainCenter');
+    let main = document.querySelector('main');
     main.append(gameBoard);
+
+    let inputs = document.querySelector('.inputs');
+    inputs.style = "display: none";
+    main.style = "flex-grow: 5";
   }
 
-  /* Func That appends player names or BOT to Taga*/
+  /* Func That appends player names or BOT to Tags*/
   
-  //Assign Event Handling to our Selection Menu + conditions for Functionality.
-  _buttons.forEach(check => check.addEventListener('click', (e) => {
-      const newPlayer = _Player('New'/*Ammend to Text Input*/, e.target.id);
-        //Position newPlayer into our array depending on the Player Number.
-        if (newPlayer.playerNum === 1){
-          players[0] = newPlayer;
-        } else if (newPlayer.playerNum === 2){
-          players[1] = newPlayer;
-        } 
-        
-        //Check our Array contains both players before creating GameBoard.
-        if (players.includes(undefined) || players.length < 2){
-          console.log("fill in players");
-        } else {
-          console.log("both players found");
-        }
-        /*Include function to check if our array has 2 players inside, and if it does, begin the game.*/
+  //Assign Event Handling to our Selection Inputs + Conditions for Deciding Game State.
+  _startButton.addEventListener('click', (e) => {
+      const inputs = document.querySelectorAll('.text');
 
-  }));
+
+      const newPlayer = _Player();
+
+  });
 return { players, gameBoard };
 
 
@@ -57,3 +50,18 @@ const GameBoard = (() => {
 
 })();
 
+
+  /* //Position newPlayer into our array depending on the Player Number.
+   if (newPlayer.playerNum === 1){
+    players[0] = newPlayer;
+  } else if (newPlayer.playerNum === 2){
+    players[1] = newPlayer;
+  } 
+  
+  //Check our Array contains both players before creating GameBoard.
+  if (players.includes(undefined) || players.length < 2){
+    console.log("fill in players");
+  } else {
+    console.log("both players found");
+  }
+  /*Include function to check if our array has 2 players inside, and if it does, begin the game.*/ */
