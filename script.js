4843cbfdba19cccb7ab62acdeb6269ed9cd5f0a4
unknown <mortson@hotmail.co.uk>
2022-07-05 21:36:27 +0100

    Created A Turn Count function for Player Objects and an Event Decider that renders X's or O's depending on who's turn it is

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
+//This Module Pattern Object handles all of our initial game creation.
 const GameSetup = (() => {
   const players = [];
+  const gameBoard = [];
   const _startButtons = document.querySelectorAll('.startButton');
   
 
  //Factory Functions that Return our Player or Computer Objects.
   const _Player = (playerName, playerNum) => {
-    let winCount = 0;
-      function addWin(){
-        winCount++;
+    
+    let _turnCount = 0;
+      function addTurn(){
+        return _turnCount++;
       }
-    return {playerName, playerNum, winCount, addWin};
+      
+    return {playerName, playerNum, addTurn };
   };
 
 
  //Inherit prototype of Player, create AI Methods.
   const _Computer = (playerName, playerNum) => {
+
     const playerSettings = _Player(playerName, playerNum);
 
     const iAmAComputer = () => { console.log(playerName) };
@@ -26,37 +31,37 @@ const GameSetup = (() => {
 
  //Create and Append 9 GameBoard Section Elements.
   function _startGame(){
-    const gameBoard = document.querySelector('.gameBoard');
+    const gameBoardContainer = document.querySelector('.gameBoard');
     for (let x = 0; x < 9; x++){
         const element = document.createElement('div');
         element.classList.add('gbSection');
-        gameBoard.append(element);
-      }
-    
+        gameBoardContainer.append(element);
+        gameBoard.push(element);
+      };
+
    //Hide Inputs and Resize Main.
     let inputContainer = document.querySelector('.inputs');
     let main = document.querySelector('main');
     inputContainer.style = "display: none";
     main.style = "flex-grow: 8";
   
-   //Render Player Info
+   //Render Player Score Tracking
     const playersContainer = document.querySelector('.playersContainer');
     playersContainer.style = "display: flex";
-    
     const playerNames = document.querySelectorAll('.playersContainer > div > :first-child');
     const playerScores = document.querySelectorAll('.playersContainer > div > :last-child');
     for (let x = 0; x < 2; x++){
       playerNames[x].textContent = `${players[x].playerName}` + '\'s Score:';
       playerScores[x].textContent = `${players[x].winCount}`;
-    }
-
+    };
   };
+
   
-//Assign Event Handling to our Start Buttons + Conditions for Deciding Game State.
+ //Assign Event Handling to our Start Buttons + Conditions for Deciding Game State.
   _startButtons.forEach(startButton => startButton.addEventListener('click', (e) => {
     e.preventDefault();
 
-  /*First Select all Form Text Fields related to our Start Buttons*/
+   /*First Select all Form Text Fields related to our Start Buttons*/
     let inputs = (() => {
         const positives = [];
           let textBoxes = document.querySelectorAll('input[type=text]');
@@ -68,7 +73,7 @@ const GameSetup = (() => {
           return positives;
     })();
 
-  /*Create our Player Objects */
+   /*Create our Player Objects */
     const createPlayers = (() => {
       for (x = 0; x < inputs.length; x++){
         let newPlayer = _Player(inputs[x], x+1);
@@ -78,20 +83,35 @@ const GameSetup = (() => {
         let newComputer = _Computer('Computer', 2) //Create our Computer Object
         players.push(newComputer);
       };
+      //Immediately Increment the first players Turn Count.
+      players[0].addTurn();
    })();
-   
-   _startGame();
-   
 
+  
+  //Finally Render Our GameBoard.
+   _startGame();
   }));
 
-return { players };
+return { players, gameBoard };
 })();
 
 
 const Game = (() => {
-  const gameBoard = document.querySelectorAll('.gbSection');
+ //When we first Mouseover our GameBoard, apply EventHandler's to each of our nine sections.
+  const bindEventApplication = document.querySelector('.gameBoard').addEventListener('mouseover', (e) => {
+      GameSetup.gameBoard.forEach(gbSection => gbSection.addEventListener('click', (e) => {
 
+         if (GameSetup.players[0].addTurn() > GameSetup.players[1].addTurn()){
+           e.target.textContent = "O";
+           GameSetup.players[1].addTurn();
+            
+         } else {
+           e.target.textContent = "X";
+           GameSetup.players[0].addTurn();
+         }
+    }));
+  }, {once: true});
+ 
   return {};
 
 })();
